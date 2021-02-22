94510d1e8d2020109a169b4e6865cd760568c1b0
Nora <nora.hamacher@gmail.com>
2021-02-22 12:49:02 +1100

    Updates

---
+++ precache-manifest.54a8e6566c8c5e41c78db49b6288f3f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150bceb87cc06181dba80aefa1be8b8f",
+    "url": "/migration_policies/index.html"
+  },
+  {
+    "revision": "7f281564c6af8972f380",
+    "url": "/migration_policies/static/css/main.e42a43bb.chunk.css"
+  },
+  {
+    "revision": "96677c5d3d43a9f555be",
+    "url": "/migration_policies/static/js/2.ce009b03.chunk.js"
+  },
+  {
+    "revision": "91879ca8440154dbe7a98f8378824817",
+    "url": "/migration_policies/static/js/2.ce009b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f281564c6af8972f380",
+    "url": "/migration_policies/static/js/main.e76a3e07.chunk.js"
+  },
+  {
+    "revision": "267e698cead4a15c5447",
+    "url": "/migration_policies/static/js/runtime-main.c7033a68.js"
+  }
+]);
